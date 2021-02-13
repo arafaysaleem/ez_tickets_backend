0f33a2be5d48058001042d126df7493f0a6594aa
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2021-02-13 19:03:36 +0500

    user endpoints refactored

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,8 +8,8 @@ const UserRole = require('../utils/userRoles.utils');
 const { createUserSchema, updateUserSchema, validateLogin } = require('../middleware/validators/userValidator.middleware');
 
 router.get('/', auth(), awaitHandlerFactory(userController.getAllUsers)); // localhost:3000/api/v1/users
-router.get('/:id', auth(), awaitHandlerFactory(userController.getUserById)); // localhost:3000/api/v1/users/1
-router.patch('/:id', auth(UserRole.Admin,UserRole.SuperUser), updateUserSchema, awaitHandlerFactory(userController.updateUser)); // localhost:3000/api/v1/users/id/1
-router.delete('/:id', auth(UserRole.Admin,UserRole.SuperUser), awaitHandlerFactory(userController.deleteUser)); // localhost:3000/api/v1/users/id/1
+router.get('/id/:id', auth(), awaitHandlerFactory(userController.getUserById)); // localhost:3000/api/v1/users/1
+router.patch('/id/:id', auth(UserRole.Admin,UserRole.SuperUser), updateUserSchema, awaitHandlerFactory(userController.updateUser)); // localhost:3000/api/v1/users/id/1
+router.delete('/id/:id', auth(UserRole.Admin,UserRole.SuperUser), awaitHandlerFactory(userController.deleteUser)); // localhost:3000/api/v1/users/id/1
 
 module.exports = router;
