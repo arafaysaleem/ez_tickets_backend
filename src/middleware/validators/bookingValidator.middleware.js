439f744f5bfa5a96bf673802acfba037a9d022af
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2021-03-05 18:35:58 +0500

    booking validator

--- src/middleware/validators/bookingValidator.middleware.js
+++ src/middleware/validators/bookingValidator.middleware.js
@@ -1,6 +1,6 @@
 const { body } = require('express-validator');
 const BookingStatus = require('../../utils/enums/bookingStatus.utils');
-const { seatRegex } = require('../../utils/common.utils');
+const { seatRegex, datetimeRegex } = require('../../utils/common.utils');
 
 exports.createBookingSchema = [
     body('user_id')
@@ -29,7 +29,12 @@ exports.createBookingSchema = [
         .exists()
         .withMessage('Ticket price is required for the booking')
         .isInt({ min: 1 })
-        .withMessage('Price should a valid whole number > 0')
+        .withMessage('Price should a valid whole number > 0'),
+    body('booking_datetime')
+        .exists()
+        .withMessage('Booking datetime is required')
+        .matches(datetimeRegex)
+        .withMessage('Booking datetime should be valid datetime of format \'YYYY-MM-DD HH:mm:ss\'')
 ];
 
 exports.updateBookingSchema = [
@@ -42,14 +47,37 @@ exports.updateBookingSchema = [
         .isString()
         .withMessage('Seat has to be a string')
         .matches(seatRegex)
-        .withMessage('Invalid seat id found. Follow format \'A-11\''),
+        .withMessage('Invalid seat id found. Follow format \'A-11\'')
+        .custom((seat, {req}) => {
+            if (req.body.filters.show_id === undefined
+                || req.body.filters.user_id === undefined
+                || req.body.filters.seat === undefined){
+                return false;
+            }
+            return true;
+        })
+        .withMessage('Please provide show_id and user_id & old seat to update seat'),
     body('price')
         .optional()
         .isInt({ min: 1 })
         .withMessage('Price should a valid whole number > 0'),
     body('filters')
         .exists()
-        .withMessage('Please enter update where conditions'),
+        .withMessage('Please enter update where conditions')
+        .custom((filters) => {
+            if (filters instanceof Object) { // if a json
+                return true;
+            }
+            return false;
+        })
+        .withMessage('Filters have to be a json')
+        .custom((filters) => {
+            if (Object.keys(filters).length !== 0){ // if no filters
+                return true;
+            }
+            return false;
+        })
+        .withMessage('Filters can\'t be empty'),
     body('filters.*')
         .notEmpty()
         .withMessage('Must specify some filters'),
@@ -65,6 +93,12 @@ exports.updateBookingSchema = [
         .optional()
         .isInt({ min: 1 })
         .withMessage('Invalid ShowID found'),
+    body('filters.seat')
+        .optional()
+        .isString()
+        .withMessage('Seat has to be a string')
+        .matches(seatRegex)
+        .withMessage('Invalid seat id found. Follow format \'A-11\''),
     body()
         .custom(value => {
             return Object.keys(value).length !== 0;
@@ -101,7 +135,11 @@ exports.bookingGetFiltersSchema = [
     body('price')
         .optional()
         .isInt({ min: 1 })
-        .withMessage('Price should a valid whole number > 0')
+        .withMessage('Price should a valid whole number > 0'),
+    body('booking_datetime')
+        .optional()
+        .matches(datetimeRegex)
+        .withMessage('Booking datetime should be valid datetime of format \'YYYY-MM-DD HH:mm:ss\'')
 ];
 
 exports.bookingDeleteFiltersSchema = [
