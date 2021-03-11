cbf64736c22d07e9f6c8c9dc0f58222f88d1a193
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2021-03-11 19:40:15 +0500

    backups updated

--- ez_tickets.sql
+++ ez_tickets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2021 at 03:06 PM
+-- Generation Time: Mar 11, 2021 at 03:34 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -46,7 +46,7 @@ INSERT INTO `bookings` (`booking_id`, `user_id`, `show_id`, `seat_row`, `seat_nu
 (1, 1, 14, 'A', 11, 700, 'confirmed', '0000-00-00 00:00:00'),
 (2, 2, 14, 'A', 16, 700, 'confirmed', '0000-00-00 00:00:00'),
 (3, 2, 14, 'A', 17, 700, 'confirmed', '0000-00-00 00:00:00'),
-(6, 2, 18, 'B', 2, 700, 'confirmed', '2021-03-05 06:41:00');
+(6, 2, 18, 'B', 2, 700, 'cancelled', '2021-03-05 06:41:00');
 
 -- --------------------------------------------------------
 
@@ -71,8 +71,9 @@ CREATE TABLE `movies` (
 
 INSERT INTO `movies` (`movie_id`, `title`, `summary`, `year`, `rating`, `trailer_url`, `poster_url`, `movie_type`) VALUES
 (1, 'GOOGLE', 'Fearsome bitches.', 2012, '0.9', 'https://www.youtube.com/watch?v=odM92ap8_c0', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpMwciKW5gytHux-4a4B7e50sQCyvnix9C57mAGnY0DNX4-uGQ', 'now_showing'),
-(4, 'FACEBOOK', 'Fearsome bitches.', 2020, '0.9', 'https://www.youtube.com/watch?v=odM92ap8_c0', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpMwciKW5gytHux-4a4B7e50sQCyvnix9C57mAGnY0DNX4-uGQ', 'coming_soon'),
-(7, 'APPLE', 'Fearsome bitches.', 2020, '0.9', 'https://www.youtube.com/watch?v=odM92ap8_c0', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpMwciKW5gytHux-4a4B7e50sQCyvnix9C57mAGnY0DNX4-uGQ', 'now_showing');
+(4, 'FACEBOOK', 'Fearsome bitches.', 2020, '0.9', 'https://www.youtube.com/watch?v=odM92ap8_c0', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpMwciKW5gytHux-4a4B7e50sQCyvnix9C57mAGnY0DNX4-uGQ', 'now_showing'),
+(7, 'APPLE', 'Fearsome bitches.', 2020, '0.9', 'https://www.youtube.com/watch?v=odM92ap8_c0', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpMwciKW5gytHux-4a4B7e50sQCyvnix9C57mAGnY0DNX4-uGQ', 'now_showing'),
+(12, 'LIONNESS', 'Fearsome lionness.', 2021, '0.9', 'https://www.youtube.com/watch?v=odM92ap8_c0', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpMwciKW5gytHux-4a4B7e50sQCyvnix9C57mAGnY0DNX4-uGQ', 'coming_soon');
 
 -- --------------------------------------------------------
 
@@ -99,20 +100,10 @@ INSERT INTO `movie_roles` (`movie_id`, `role_id`, `role_type`) VALUES
 (4, 3, 'cast'),
 (7, 1, 'producer'),
 (7, 2, 'director'),
-(7, 3, 'cast');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `otp_codes`
---
-
-CREATE TABLE `otp_codes` (
-  `user_id` int(10) UNSIGNED NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `OTP` text NOT NULL,
-  `expiration_datetime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(7, 3, 'cast'),
+(12, 1, 'producer'),
+(12, 2, 'director'),
+(12, 3, 'cast');
 
 -- --------------------------------------------------------
 
@@ -244,8 +235,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `full_name`, `email`, `password`, `address`, `contact`, `role`) VALUES
 (1, 'Test Admin', 'admin@gmail.com', '$2a$08$y4hYovfviz31AZhru3t4FObYrLFrhdBNETXikx6WFD9VYGbUZucYO', 'Karachi', '+923009999999', 'admin'),
 (2, 'Test User', 'user@gmail.com', '$2a$08$V5FWrXIoHeJPGe0WDgQcJ.eJEX.rz6tCtevaNSKBH6deaoVDKMosS', 'Karachi', '+923009999999', 'api_user'),
-(10, 'Email Test User', 'arafaysaleem@gmail.com', '$2a$08$NMklGbvfLkOwYgo1lB6qbOxGUmOuH4gymNwU6WXC4H2gTa39v9OPW', 'Karachi', '+923009999999', 'api_user'),
-(11, 'Test User', 'user2@gmail.com', '$2a$08$dP89ppGNiMczZM0JToQwF.WtiR53NL0j.r55wBNJP03sjDh/VpcrC', 'Karachi', '+923009999999', 'api_user');
+(10, 'Email Test User', 'arafaysaleem@gmail.com', '$2a$08$YcMueJpi26hS9jqLII7n4ep/pPYwNhiS/OfISftVg9M0tfIzYzUvO', 'Karachi', '+923009999999', 'api_user'),
+(11, 'Test User', 'user2@gmail.com', '$2a$08$dP89ppGNiMczZM0JToQwF.WtiR53NL0j.r55wBNJP03sjDh/VpcrC', 'Karachi', '+923009999999', 'api_user'),
+(14, 'Test User 3', 'user3@gmail.com', '$2a$08$hyIqip2MuwqbmkKIoiWG9eihI0HkAF0aB.3aWVUBO0YiBPIBawEDK', 'Karachi', '+923009999999', 'api_user');
 
 --
 -- Indexes for dumped tables
@@ -272,13 +264,6 @@ ALTER TABLE `movies`
 ALTER TABLE `movie_roles`
   ADD PRIMARY KEY (`movie_id`,`role_id`) USING BTREE,
   ADD KEY `fk_mroles_role_id` (`role_id`);
-
---
--- Indexes for table `otp_codes`
---
-ALTER TABLE `otp_codes`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `roles`
@@ -323,19 +308,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `booking_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `booking_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
-  MODIFY `movie_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `otp_codes`
---
-ALTER TABLE `otp_codes`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `movie_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -359,7 +338,7 @@ ALTER TABLE `theaters`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
@@ -378,12 +357,6 @@ ALTER TABLE `bookings`
 ALTER TABLE `movie_roles`
   ADD CONSTRAINT `fk_mroles_movie_id` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`),
   ADD CONSTRAINT `fk_mroles_role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`);
-
---
--- Constraints for table `otp_codes`
---
-ALTER TABLE `otp_codes`
-  ADD CONSTRAINT `fk_otp_codes_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `shows`
