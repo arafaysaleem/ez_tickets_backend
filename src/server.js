d5ccf5cb43bb4d508ced9e345c3afeb151703fe9
arafaysaleem <a.rafaysaleem@gmail.com>
2021-08-03 11:28:57 -0700

    style(Server.js): improve logging of server launch

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const { ExpressLoader } = require('./loaders/express.loader');
 const { DatabaseLoader } = require('./loaders/database.loader');
 const { RoutesLoader } = require('./loaders/routes.loader');
 const { MiddlewareLoader } = require('./loaders/middleware.loader');
-const { Config } = require('../../configs/config');
+const { Config } = require('../configs/config');
 
 // load express
 const app = ExpressLoader.init();
@@ -19,6 +19,10 @@ MiddlewareLoader.init(app);
 
 // starting the server
 const port = Number(Config.PORT);
-app.listen(port, () => console.log(`🚀 Server running on port ${port}!`));
+app.listen(port, () => console.log(`
+  ==================================
+  🚀 Server running on port ${port}!🚀
+  ==================================
+`));
 
 module.exports = app;
