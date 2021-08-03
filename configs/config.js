d72289c60c4e99af37060e63b1c70325aba04898
arafaysaleem <a.rafaysaleem@gmail.com>
2021-08-03 07:50:13 -0700

    add(Config): Move env vars to a config file

---
+++ configs/config.js
@@ -0,0 +1,11 @@
+module.exports.Config = {
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 3331,
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PASS: process.env.DB_PASS || '',
+    DB_DATABASE: process.env.DB_DATABASE || 'test',
+    SECRET_JWT: process.env.SECRET_JWT || "",
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY || "SENDGRID_API_KEY",
+    SENDGRID_SENDER: process.env.SENDGRID_SENDER || "FROM_EMAIL"
+};
